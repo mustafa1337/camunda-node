5fcf46c68b55de6d1af3b228bd5d680aa2141459
mustafa1337 <98588878+mustafa1337@users.noreply.github.com>
2024-01-27 16:36:16 +0100

    E-Mail send test_script.mjs

--- test_script.mjs
+++ test_script.mjs
@@ -1,26 +1,11 @@
 import { Client, logger } from "camunda-external-task-client-js";
 import { Variables } from "camunda-external-task-client-js";
+import nodemailer from 'nodemailer';
 
 const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
 const client = new Client(config);
 
-/*
-client.subscribe("calculate-due-date", async function({ task, taskService }) {
-	const startDate = task.variables.get("start_date");
-	const type = task.variables.get("type");
-	let numWeeks = 9;
-	if (type == 'master') {numWeeks = 22};
-	let dueDate = new Date();
-    dueDate.setDate(startDate.getDate() + numWeeks * 7);
-	const processVariables = new Variables();
-    processVariables.set("due_date", dueDate);
-	
-    await taskService.complete(task, processVariables);
-}); 
-*/
 
-
-// Füge Log-Statements hinzu
 client.subscribe("calculate-due-date", async function({ task, taskService }) {
     console.log("Processing task:", task);
 
@@ -44,3 +29,31 @@ client.subscribe("calculate-due-date", async function({ task, taskService }) {
     console.log("Task completed");
 });
 
+
+client.subscribe("email_versenden", async function({ task, taskService }) {
+    var transporter = nodemailer.createTransport({
+        service: 'ethereal',
+        auth: {
+          user: 'jamel.oreilly@ethereal.email',
+          pass: 'uMWRV5EQWNp6yDg34N'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'jamel.oreilly@ethereal.email',
+        to: 'jamel.oreilly@ethereal.email',
+        subject: 'Sending Email using Node.js',
+        text: 'Sending a mail with NodeJS yeeaa!'
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+    
+    await taskService.complete(task, processVariables);
+    console.log("Task completed");
+});
